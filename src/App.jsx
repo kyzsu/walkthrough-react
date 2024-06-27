a1c3cf756e462d380020d5f24db16505f06412e0
kyzsu <l0calh34rtz@gmail.com>
2024-06-27 14:34:17 +0700

    feat: implement react-query & details

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,32 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 
 import SearchParams from './SearchParams';
 import Details from './Details';
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: Infinity,
+      cacheTime: Infinity,
+    },
+  },
+});
+
 const App = () => {
   return (
     <BrowserRouter>
-      <header>
-        <Link to="/">Adopsi saya!</Link>
-      </header>
-      <Routes>
-        <Route path="/" element={<SearchParams />} />
-        <Route path="/details/:id" element={<Details />} />
-      </Routes>
+      <QueryClientProvider client={queryClient}>
+        <header>
+          <Link to="/">Adopsi saya!</Link>
+        </header>
+        <Routes>
+          <Route path="/" element={<SearchParams />} />
+          <Route path="/details/:id" element={<Details />} />
+        </Routes>
+      </QueryClientProvider>
     </BrowserRouter>
   );
 };
--- src/Details.jsx
+++ src/Details.jsx
@@ -1,8 +1,36 @@
 import { useParams } from 'react-router-dom';
+import { CircleLoader } from 'react-spinners';
+import { useQuery } from '@tanstack/react-query';
+
+import fetchPet from './fetchPet';
 
 const Details = () => {
   const params = useParams();
-  return <div>Details {params.id}</div>;
+  const results = useQuery(['details', params.id], fetchPet);
+  // useQuery(1 = payload/options merupakan array => [a = label,b = id], 2 = function)
+
+  if (results.isLoading) {
+    return (
+      <div className="loading-pane">
+        <h2 className="loader">
+          <CircleLoader size={40} color="#0000ff" />
+        </h2>
+      </div>
+    );
+  }
+
+  const pet = results.data.pets[0];
+
+  return (
+    <div className="details">
+      <div>
+        <h1>{pet.name}</h1>
+        <h2>{`${pet.animal} ${pet.breed} ${pet.city}, ${pet.state}`}</h2>
+        <button>Apakah anda mau meng-adopsi {pet.name}?</button>
+        <p>{pet.description}</p>
+      </div>
+    </div>
+  );
 };
 
 export default Details;
