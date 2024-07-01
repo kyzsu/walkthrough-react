ebda6f3a5db1b8349be2d200606872c7d68abed0
kyzsu <l0calh34rtz@gmail.com>
2024-07-01 15:50:58 +0700

    feat: implement SSR (notfix)

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
-import { createRoot } from 'react-dom/client';
+import { Routes, Route, Link } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 
 import Details from './Details';
 import SearchParams from './SearchParams';
@@ -19,22 +18,18 @@ const queryClient = new QueryClient({
 const App = () => {
   const adoptedPet = useState(null);
   return (
-    <BrowserRouter>
-      <AdoptedPetContext.Provider value={adoptedPet}>
-        <QueryClientProvider client={queryClient}>
-          <header>
-            <Link to="/">Adopsi saya!</Link>
-          </header>
-          <Routes>
-            <Route path="/" element={<SearchParams />} />
-            <Route path="/details/:id" element={<Details />} />
-          </Routes>
-        </QueryClientProvider>
-      </AdoptedPetContext.Provider>
-    </BrowserRouter>
+    <AdoptedPetContext.Provider value={adoptedPet}>
+      <QueryClientProvider client={queryClient}>
+        <header>
+          <Link to="/">Adopsi saya!</Link>
+        </header>
+        <Routes>
+          <Route path="/" element={<SearchParams />} />
+          <Route path="/details/:id" element={<Details />} />
+        </Routes>
+      </QueryClientProvider>
+    </AdoptedPetContext.Provider>
   );
 };
 
-const container = document.getElementById('root');
-const root = createRoot(container);
-root.render(<App />);
+export default App;
